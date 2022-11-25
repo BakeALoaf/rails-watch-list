2aee75f22a2e6fb10063d9722e4d66ff795a408d
James Barker <jbarkerlouth@gmail.com>
2022-11-25 16:06:47 +0100

    working on front end

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,5 +7,5 @@ import { application } from "./application"
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
-import TomSelectController from "./tom_select_controller"
-application.register("tom-select", TomSelectController)
+import NavbarController from "./navbar_controller"
+application.register("navbar", NavbarController)
